c8eef777d7a9c13ce094c16b0a9a91a2d10145ca
Jodsonc <jodsoncalabria@hotmail.com>
2021-11-20 21:08:05 +0000

    Commit initial
    Commit initial

---
+++ src/br/com/cursojava/controller/PrimeiraAula.java
@@ -0,0 +1,18 @@
+package br.com.cursojava.controller;
+
+public class PrimeiraAula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nome = "Jodson Calabria";
+		int n1 = 2;
+		int n2 = 4;
+		int resultado = n1*n2;
+System.out.println(nome.trim());
+System.out.println(resultado);
+		
+		
+	}
+
+}
